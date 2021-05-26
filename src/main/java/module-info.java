18efc7128e02480efd61fe35ff4299ed246e32cc
DeboloAurelien <auredebolo@gmail.com>
2021-05-26 15:19:37 +0200

    Connection with SQL server established

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fr.epsi {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens fr.epsi to javafx.fxml;
     opens fr.epsi.controllers to javafx.fxml;
